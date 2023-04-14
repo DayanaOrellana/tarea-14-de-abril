534e8e91dbaadd7820bbfe225f99c2e7d0316904
Dayana Escobar <dorellanae2@miumg.edu.gt>
2023-04-13 19:18:24 -0600

    Agregar archivos de proyecto.

---
+++ tarea 14 de abril/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+    void Promedio()
+    {
+       
+        Console.WriteLine("Ingrese su Nombre:");
+        string nom = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su Nota:");
+        double not1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese su Nota:");
+        double not2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese su Nota:");
+        double not3 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese su Nota:");
+       double not4 = double.Parse(Console.ReadLine());
+    
+    double promedio = (not1 + not2 + not3 + not4) / 4;
+       
+
+        Console.WriteLine("" + nom);
+        Console.WriteLine("El promedio de su notas es: " + promedio);
+
+        
+    }
+Promedio();
+
+
